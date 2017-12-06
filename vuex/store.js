1d29ba40bc4ba7ba99e39ef75be9e37c95bfbe27
wangjun <wangjun02@sodao.com>
2017-12-06 14:23:59 +0800

    目测不用es6很难映射呀

--- vuex/store.js
+++ vuex/store.js
@@ -4,7 +4,7 @@ define(function(require, exports, module) {
 		state: {
 			count: 0
 		},
-		// 通过提交mutation，我们可以在应用程序中根据mutation的名字来判断状态的变化过程。
+		// 通过提交mutation，我们可以在应用程序中根据mutation的名字来判断状态的变化过程。,状态的变化是同步的
 		mutations: {
 			increment (state) {
 				state.count ++
@@ -12,6 +12,19 @@ define(function(require, exports, module) {
 			reduce (state) {
 				state.count --
 			}
+		},
+		// action类似于mutation,不同的是action提交的是mutation,而不是直接变更状态,action可以包含任意异步操作。动作的发生可以是异步的
+		actions: {
+			incre(context) {
+				setTimeout(function(){
+					context.commit('increment');
+				},2000)
+			}
+		},
+		getters: {
+		    getstate: function(state) {
+		    	return state
+		    }
 		}
 	})
 	console.log(store.state.count);
@@ -19,6 +32,8 @@ define(function(require, exports, module) {
 	console.log(store.state.count);
 	store.commit('increment'); //又加了一次
 	console.log(store.state.count);
-
+	store.dispatch('incre');   //通过action来进行
+	console.log(store.state.count);
+	console.log(store.getters.getstate);
 	return store;
 })
